bfe5ec36badd4b9119b8becd67c7007839940aad
ryzen <ryzensingh@gmail.com>
2022-05-17 00:19:57 +0530

    file structure updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route }
+  from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
-
-export default App;
+import Home from './Pages/home';
+import Events from './Pages/events';
