018eaa151f4da9616e6f6af970d257630e312ec4
Harshit Sharma <75475819+harshit-sharma-gits@users.noreply.github.com>
2022-05-18 11:13:08 +0530

    Update home.js

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,16 +1,1 @@
-import React from "react";
-
-
-
-const Home = () => {
-    return (
-        <>
-            <div>
-                <h5>Hey Buddy</h5>
-            </div>
-        </>
-    )
-}
-
-export default Home
 
